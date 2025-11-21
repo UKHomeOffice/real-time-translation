08fb57e94aa460c93a944f7a349c63ba82d65635
Lewis Miller <lewis.miller8@homeoffice.gov.uk>
2025-11-21 10:46:17 +0000

    Fix linting issues in inference_example.py

--- src/inference_example.py
+++ src/inference_example.py
@@ -1,3 +1,4 @@
+"""Example script to test the deployed SageMaker endpoint"""
 import json
 
 import boto3
@@ -8,6 +9,7 @@ KEY_NAME = "PLACEHOLDER"
 
 
 def main() -> None:
+    """Tests the deployed SageMaker endpoint by invoking it with an example audio file"""
     sagemaker_runtime_client = boto3.client("sagemaker-runtime")
     body_data = json.dumps({
         "processedFileBucket": BUCKET_NAME,
@@ -17,9 +19,11 @@ def main() -> None:
         "return_timestamps": False,
         "task": "translate"
     }).encode("utf-8")
-    response = sagemaker_runtime_client.invoke_endpoint(EndpointName="real-time-translation-whisper",
-                                             Body=body_data,
-                                             ContentType="application/json")
+    response = sagemaker_runtime_client.invoke_endpoint(
+        EndpointName="real-time-translation-whisper",
+        Body=body_data,
+        ContentType="application/json",
+    )
     print(response["Body"].read().decode("utf-8"))
 
 
