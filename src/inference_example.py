fc3793467f685816ec8fae86e66985f1eb3b701a
Lewis Miller <lewis.miller8@homeoffice.gov.uk>
2025-11-21 10:39:35 +0000

    Add inference example and requirements files

---
+++ src/inference_example.py
@@ -0,0 +1,27 @@
+import json
+
+import boto3
+
+
+BUCKET_NAME = "PLACEHOLDER"
+KEY_NAME = "PLACEHOLDER"
+
+
+def main() -> None:
+    sagemaker_runtime_client = boto3.client("sagemaker-runtime")
+    body_data = json.dumps({
+        "processedFileBucket": BUCKET_NAME,
+        "processedFileKey": KEY_NAME,
+        "language": "auto",
+        "return_language": False,
+        "return_timestamps": False,
+        "task": "translate"
+    }).encode("utf-8")
+    response = sagemaker_runtime_client.invoke_endpoint(EndpointName="real-time-translation-whisper",
+                                             Body=body_data,
+                                             ContentType="application/json")
+    print(response["Body"].read().decode("utf-8"))
+
+
+if __name__ == "__main__":
+    main()
